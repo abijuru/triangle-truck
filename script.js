6fdf7f47c0ee52564c44b2752e1ef47197cbd63f
abijuru <abijuruelisa@gmail.com>
2018-12-15 01:30:42 -0500

    add the var of my function

---
+++ script.js
@@ -0,0 +1,24 @@
+function myTruck () {
+var side1=parseInt(document.getElementById("1st side").value)
+var side2=parseInt(document.getElementById("2and side").value)
+var  side3=parseInt(document.getElementById("3rd side").value)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
