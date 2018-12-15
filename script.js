797beffc8253203fb8a61a7e27fc8c78d7929bad
abijuru <abijuruelisa@gmail.com>
2018-12-15 02:03:09 -0500

    create if statement in js

--- script.js
+++ script.js
@@ -3,21 +3,25 @@ var side1=parseInt(document.getElementById("1st side").value)
 var side2=parseInt(document.getElementById("2and side").value)
 var  side3=parseInt(document.getElementById("3rd side").value)
 
+if (side1 === side2 && side1===side3 &&  side3===side )is {
+   alret("equilateralTruck!");
 
+}
+ else if(side1 === side2 || side1===side3 || side3===side2) is
 
+ {
+alret("IsoscelesTruck!");
 
+ }
 
+else if(side1 !== side2 && side1 !==side3 && side3 !== side2  {
 
+alert("scaleneTruck!")
+}
 
-
-
-
-
-
-
-
-
-
+else{
+alret("not Truck")
+}
 
 
 
