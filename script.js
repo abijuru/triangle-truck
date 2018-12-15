b41c8ed811fa09cb83bb320a253cb7823b0a6917
abijuru <abijuruelisa@gmail.com>
2018-12-15 02:30:53 -0500

    i correct statment

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 function myTruck () {
-var side1=parseInt(document.getElementById("1st side").value)
-var side2=parseInt(document.getElementById("2and side").value)
-var  side3=parseInt(document.getElementById("3rd side").value)
+var side1=parseInt(document.getElementById("1st side").value);
+var side2=parseInt(document.getElementById("2and side").value);
+var  side3=parseInt(document.getElementById("3rd side").value);
 
-if (side1 === side2 && side1===side3 &&  side3===side )is {
+if (side1 === side2 && side1===side3 &&  side3===side2 ) {
    alret("equilateralTruck!");
 
 }
- else if(side1 === side2 || side1===side3 || side3===side2) is
+ else if(side1 === side2 || side1===side3 || side3===side2) 
 
  {
-alret("IsoscelesTruck!");
+alret("Isosceles");
 
  }
 
-else if(side1 !== side2 && side1 !==side3 && side3 !== side2  {
+else if(side1 !== side2 && side1 !==side3 && side3 !== side2 ) {
 
-alert("scaleneTruck!")
+alert("scalene");
 }
 
 else{
-alret("not Truck")
+alret("not a triangle");
 }
 
 
