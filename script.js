a5e52a6ab85c04286af247dc47e0df345fc98980
abijuru <abijuruelisa@gmail.com>
2018-12-15 03:55:53 -0500

    create a new statement

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-function myTruck () {
-var side1=parseInt(document.getElementById("first side").value);
-var side2=parseInt(document.getElementById("second side").value);
-var  side3=parseInt(document.getElementById("thrid side").value);
+function myTriangle () {
+var side1 =parseInt(document.getElementById("side1").value);
+var side2 = parseInt (document.getElementById("side2").value);
+var  side3 = parseInt ( document.getElementById("side3").value);
 
 if (side1 === side2 && side1===side3 &&  side3===side2 ) {
-   alret("equilateralTruck!");
+   alert("that triangle is  equilateral ");
 
 }
- else if(side1 === side2 || side1===side3 || side3===side2) 
+ else if(side1 === side2 && side1!==side3 && side3!==side2) 
 
  {
-alret("Isosceles");
+alert("that triangle is  Isosceles");
 
  }
 
 else if(side1 !== side2 && side1 !==side3 && side3 !== side2 ) {
 
-alert("scalene");
+alert(" that triangle is scalene");
 }
 
 else{
-alret("not a triangle");
+alert(" that is not a triangle");
 }
 
 
-
+}
 
